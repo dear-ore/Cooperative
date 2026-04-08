ee16e90c7f97e28c97f9a1883bd08de4b57b299b
dear-ore <adorable2209@gmail.com>
2026-04-08 17:07:28 +0100

    Finished all 5 endpoints for executives controller

--- Cooperative/Controllers/ExecutivesController.cs
+++ Cooperative/Controllers/ExecutivesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Cooperative.Models;
 using Cooperative.Data;
+using Cooperative.DTOs;
 
 namespace Cooperative.Controllers
 {
@@ -34,8 +35,7 @@ namespace Cooperative.Controllers
         }
 
         [HttpPost]
-
-        public ActionResult AddNewExecutives([FromBody] Executive newExecutive)
+        public ActionResult AddNewExecutives([FromBody] AddExecutivesDto newExecutive)
         {
             var executiveObj = new Executive();
             executiveObj.Name = newExecutive.Name;
@@ -45,5 +45,38 @@ namespace Cooperative.Controllers
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetExecutivesById), new { id = executiveObj.Id }, executiveObj);
         }
+
+        [HttpPut("{id}")]
+        public ActionResult EditExecutive([FromBody] AddExecutivesDto editedExecutive, int id)
+        {
+            var executive = _context.Executives.FirstOrDefault(e => e.Id == id);
+            if(executive == null)
+            {
+                return NotFound();
+            }
+
+            var executiveObj = new Executive();
+            executiveObj.Name = editedExecutive.Name;
+            executiveObj.PhoneNumber = editedExecutive.PhoneNumber;
+            executiveObj.Email = editedExecutive.Email;
+            executiveObj.PostHeld = editedExecutive.PostHeld;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id})")]
+        public ActionResult DeleteExecutive(int id)
+        {
+            var executive = _context.Executives.FirstOrDefault(e => e.Id == id);
+            if(executive == null)
+            {
+                return NotFound();
+            }
+
+            _context.Executives.Remove(executive);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
---
+++ Cooperative/DTOs/AddExecutivesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Cooperative.DTOs
+{
+        public class AddExecutivesDto
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public string PostHeld { get; set; }
+        }
+
+}
