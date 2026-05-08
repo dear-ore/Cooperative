0f46b70d0c9ecfd92b41d1d06a70f01f7652f3ef
dear-ore <adorable2209@gmail.com>
2026-05-08 11:46:13 +0100

    Set up Loan method tests with XUnit framework.

---
+++ CooperativeTests/DebtServiceTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Cooperative.Data;
+using Cooperative.Models;   
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CooperativeTests
+{
+    public class DebtServiceTests
+    {
+        private Cooperator CreateTestCooperator(CooperatorStatus status = CooperatorStatus.Active, DateTime startDate = default)
+        {
+            return new Cooperator
+            {
+                Name = "Test User",
+                Status = status,
+                CoopNumber = "123",
+                StaffNumber = "12345",
+                Factory = "Yale 5",
+                Department = "Manufacturing",
+                PostHeld = "Parker",
+                MembershipCommencementDate = startDate,
+                DateOfEmployment = DateTime.Now.AddYears(-2),
+                IsMemberOfSimilarSociety = false,
+                MaritalStatus = "Single",
+                MonthlyContribution = 100,
+                BuildingFundBalance = 0,
+                InvestmentBalance = 0,
+                SharesBalance = 0,
+                LoanBalance = 0,
+                FoodBalance = 0,
+                SouvenirBalance = 0,
+                OtherBalance = 0
+
+            };
+        }
+        private CooperativeDbContext CreateDbContext()
+        {
+            var dbName = $"CoopTest_{Guid.NewGuid()}";
+
+            var options = new DbContextOptionsBuilder<CooperativeDbContext>()
+                .UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database={dbName};Trusted_Connection=True;")
+                .Options;
+
+            var context = new CooperativeDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        [Fact]
+        public async Task TakeLoan_IsSuccessIsFalse_WhenCooperator_NotFound()
+        {
+            //Arrange
+            var context = CreateDbContext();
+            var service = new Cooperative.Services.DebtService(context);
+
+            //Act
+            var loan = await service.TakeLoan(10000, 23, TransactionType.BankTransfer);
+
+            //Assert
+            Assert.False(loan.IsSuccess);
+        }
+
+        [Fact]
+        public async Task TakeLoan_IsSuccessIsFalse_WhenCooperator_NotActive()
+        {
+            var context = CreateDbContext();
+            var service = new Cooperative.Services.DebtService(context);
+            var cooperator = CreateTestCooperator(status: CooperatorStatus.Inactive);
+
+            context.Add(cooperator);
+            await context.SaveChangesAsync();
+
+            //Act
+            var loan = await service.TakeLoan(10000, cooperator.Id, TransactionType.BankTransfer);
+
+            //Assert
+            Assert.False(loan.IsSuccess);
+        }
+
+        [Fact]
+        public async Task TakeLoan_IsSuccessIsFalse_WhenCommencementDateIsLessThanSixMonths()
+        {
+            //Arrange
+            var context = CreateDbContext();
+            var service = new Cooperative.Services.DebtService(context);
+            var cooperator = CreateTestCooperator(startDate: DateTime.Now.AddMonths(-3));
+
+            context.Add(cooperator);
+            await context.SaveChangesAsync();
+
+            //Act
+            var loan = await service.TakeLoan(10000, cooperator.Id, TransactionType.BankTransfer);
+
+
+            //Assert
+            Assert.False(loan.IsSuccess);
+        }
+
+        [Fact]
+        public async Task TakeLoan_IsSuccessIsFalse_WhenThereIsAnActiveLoan()
+        {
+            //Arrange
+            var context = CreateDbContext();
+            var service = new Cooperative.Services.DebtService(context);
+            var cooperator = CreateTestCooperator(startDate: DateTime.Now.AddMonths(-8));
+            context.Add(cooperator);
+            await context.SaveChangesAsync();
+
+            var loan = new Loan
+            {
+                CooperatorId = cooperator.Id,
+                PrincipalAmount = 10000,
+                TotalRepayable = 11000,
+                MonthlyInstallment = 1000,
+                DateTaken = DateTime.Now.AddMonths(-1),
+                InstallmentsRemaining = 11,
+                LoanTransactionType = TransactionType.BankTransfer
+            };
+            context.Add(loan);
+            await context.SaveChangesAsync();
+
+            //Act
+            var takeLoan = await service.TakeLoan(10000, cooperator.Id, TransactionType.BankTransfer);
+
+            //Assert
+            Assert.False(takeLoan.IsSuccess);
+        }
+
+        [Fact]
+        public async Task TakeLoan_IsSuccessIsTrue_WhenAllConditionsAreMet()
+        {
+            //Arrange
+            var context = CreateDbContext();
+            var service = new Cooperative.Services.DebtService(context);
+            var cooperator = CreateTestCooperator(startDate: DateTime.Now.AddMonths(-8));
+            context.Add(cooperator);
+            await context.SaveChangesAsync();
+
+            //Act
+            var loan = await service.TakeLoan(100000, cooperator.Id, TransactionType.BankTransfer);
+
+            //Assert
+            Assert.True(loan.IsSuccess);
+        }
+    }
+}
