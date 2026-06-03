07f4f84114cbcc64f13d3eae8801842e4fc1ca3d
dear-ore <adorable2209@gmail.com>
2026-06-03 17:34:10 +0100

    Wrote controller and Dto for DebtServices

--- Cooperative/Controllers/DebtsController.cs
+++ Cooperative/Controllers/DebtsController.cs
@@ -1,6 +1,8 @@
 ﻿using Cooperative.Services;
+using Cooperative.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Cooperative.DTOs;
 
 namespace Cooperative.Controllers
 {
@@ -9,10 +11,86 @@ namespace Cooperative.Controllers
     public class DebtsController : ControllerBase
     {
         private readonly IDebtService _service;
-
+        
         public DebtsController(IDebtService service)
         {
             _service = service;
         }
+
+        [HttpPost("Take-Loan")]
+        public async Task<IActionResult> TakeLoan([FromBody] TakeLoanDto request)
+        {
+            var result = await _service.TakeLoan(
+                request.Amount,
+                request.Id,
+                request.Transaction
+            );
+
+            if(result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("Take-Food")]
+        public async Task<IActionResult> TakeFood([FromBody] TakeFoodDto request)
+        {
+            var result = await _service.TakeFood(
+                request.Amount,
+                request.Id,
+                request.NumberOfInstallments,
+                request.Description,
+                request.ReceiptNumber
+            );
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("Take-Souvenir")]
+        public async Task<IActionResult> TakeSouvenir([FromBody] TakeSouvenirDto request)
+        {
+            var result = await _service.TakeSouvenir(
+                request.Amount,
+                request.Id,
+                request.Description,
+                request.NumberOfInstallments
+            );
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("Make-Repayment")]
+        public async Task<IActionResult> MakeRepayment([FromBody] MakeRepaymentDto request)
+        {
+            var result = await _service.MakeRepayment(
+                request.LoanAmount,
+                request.SouvenirAmount,
+                request.FoodAmount,
+                request.Id,
+                request.PayMethod,
+                request.ReceiptNumber
+
+            );
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+            
     }
 }
---
+++ Cooperative/DTOs/DebtServiceDto.cs
@@ -0,0 +1,36 @@
+﻿using Cooperative.Models;
+
+namespace Cooperative.DTOs
+{
+    public class TakeLoanDto
+    {
+        public int Id { get; set; }
+        public decimal Amount{ get; set; }
+        public TransactionType Transaction { get; set; }
+    }
+    public class TakeFoodDto
+    {
+        public decimal Amount { get; set; }
+        public int Id { get; set; }
+        public int NumberOfInstallments { get; set; }
+        public string Description { get; set; }
+        public int ReceiptNumber { get; set; }
+    }
+    public class TakeSouvenirDto
+    {
+        public decimal Amount { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int NumberOfInstallments { get; set; }
+    }
+
+    public class MakeRepaymentDto
+    {
+        public decimal? LoanAmount { get; set; }
+        public decimal? SouvenirAmount { get; set; }
+        public decimal? FoodAmount { get; set; }
+        public int Id { get; set; }
+        public PaymentMethod PayMethod { get; set; }
+        public int ReceiptNumber { get; set; }
+    }
+}
