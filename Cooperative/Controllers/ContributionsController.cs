82f6a6775dab487740af8bee43b569e44391a4b1
dear-ore <adorable2209@gmail.com>
2026-07-05 15:28:41 +0100

    Contribution controller

---
+++ Cooperative/Controllers/ContributionsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Cooperative.DTOs;
+using Cooperative.Services;
+
+namespace Cooperative.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContributionsController : ControllerBase
+    {
+        private readonly IContributionService _contributionService;
+        public ContributionsController(IContributionService contributionService)
+        {
+            _contributionService = contributionService;
+        }
+
+        // POST: api/Contributions
+        [HttpPost]
+        public async Task<IActionResult> RecordContribution([FromBody] ContributionDto contributionDto)
+        {
+            var result = await _contributionService.RecordContribution(
+                contributionDto.CooperatorId,
+                contributionDto.BuildingAmount,
+                contributionDto.SharesAmount,
+                contributionDto.InvestmentAmount,
+                contributionDto.SavingsAmount
+            );
+
+            return StatusCode(result.StatusCode, result.Message);
+        }  
+    }
+}
---
+++ Cooperative/DTOs/ContributionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Cooperative.DTOs
+{
+    public class ContributionDto
+    {
+          public int CooperatorId { get; set; }
+          public decimal? BuildingAmount { get; set; }
+          public decimal? SharesAmount { get; set; }
+          public decimal? InvestmentAmount { get; set; }
+          public decimal? SavingsAmount { get; set; }
+    }
+}
